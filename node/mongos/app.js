e1153bd8afb4752ca3effe1bd6a171f0fc376cdb
JunHyen Mun <impelfin@gmail.com>
2022-01-09 18:39:22 +0900

    .

--- node/mongos/app.js
+++ node/mongos/app.js
@@ -11,8 +11,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3000)
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,9 +23,6 @@ mongoose.connect('mongodb://localhost:27017/mydb');
 
 var mongo = require('./routes/mongo.js');
 app.use('/', mongo);
-
-var input = require('./mongoose.html');
-app.use('/input', input)
 
 app.listen(app.get('port'), () =>{
 	console.log('서버 실행 중')
--- node/mongos/routes/mongo.js
+++ node/mongos/routes/mongo.js
@@ -13,42 +13,22 @@ var userSchema = mongoose.Schema({
 // create model with mongodb collection & scheme
 var User = mongoose.model('users',userSchema);
 
-// select city from users where userid='terry'
-User.findOne({'userid':'terry'}).select('city').exec(function(err,user){
-      console.log("q1");
-      console.log(user+"\n");
-      return;
+// list
+router.get('/list', function(req, res, next) {
+      User.find({},function(err,docs){
+           if(err) console.log('err');
+           res.send(docs);
+      });
 });
 
-// select * from users where city='seoul' order by userid limit 5
-User.find({'city':'seoul'}).sort({'userid':1}).limit(5).exec(function(err,users){
-      console.log("q2");
-      console.log(users+"\n");
-      return;
-});
-
-// using JSON doc query
-// select userid,age from users where city='seoul' and age > 10 and age < 29
-User.find({'city':'seoul', 'age':{$gt:10 , $lt:29}})
-      .sort({'age':-1})
-      .select('userid age')
-      .exec(function(err,users){
-           console.log("q3");
-           console.log(users+"\n");
-           return;
-});
-
-//using querybuilder
-//select userid,age from users where city='seoul' and age > 10 and age < 29
-User.find({})
-      .where('city').equals('seoul')
-      .where('age').gt(10).lt(29)
-      .sort({'age':-1})
-      .select('userid age')
-      .exec(function(err,users){
-           console.log("q4");
-           console.log(users+"\n");
-           return;
+// get
+router.get('/get', function(req, res, next) {
+      db = req.db;
+      var userid = req.query.userid
+      User.findOne({'userid':userid},function(err,doc){
+           if(err) console.log('err');
+            res.send(doc);
+      });
 });
 
 // insert
@@ -92,22 +72,56 @@ router.post('/update', function(req, res, next) {
       });
 });
 
-// list
-router.get('/list', function(req, res, next) {
-      User.find({},function(err,docs){
-           if(err) console.log('err');
-           res.send(docs);
-      });
-});
-
-// get
-router.get('/get', function(req, res, next) {
-      db = req.db;
-      var userid = req.query.userid
-      User.findOne({'userid':userid},function(err,doc){
-           if(err) console.log('err');
-            res.send(doc);
-      });
+// delte
+router.post('/delete', function(req, res, next) {
+      var userid = req.body.userid;
+      var user = User.find({'userid':userid});
+      user.remove(function(err){
+             if(err){
+                console.log(err);
+                res.status(500).send('update error');
+                return;
+             }
+             res.status(200).send("Removed");
+         });
 });
 
 module.exports = router;
+
+// select city from users where userid='terry'
+User.findOne({'userid':'terry'}).select('city').exec(function(err,user){
+      console.log("q1");
+      console.log(user+"\n");
+      return;
+});
+
+// select * from users where city='seoul' order by userid limit 5
+User.find({'city':'seoul'}).sort({'userid':1}).limit(5).exec(function(err,users){
+      console.log("q2");
+      console.log(users+"\n");
+      return;
+});
+
+// using JSON doc query
+// select userid,age from users where city='seoul' and age > 10 and age < 29
+User.find({'city':'seoul', 'age':{$gt:10 , $lt:29}})
+      .sort({'age':-1})
+      .select('userid age')
+      .exec(function(err,users){
+           console.log("q3");
+           console.log(users+"\n");
+           return;
+});
+
+//using querybuilder
+//select userid,age from users where city='seoul' and age > 10 and age < 29
+User.find({})
+      .where('city').equals('seoul')
+      .where('age').gt(10).lt(29)
+      .sort({'age':-1})
+      .select('userid age')
+      .exec(function(err,users){
+           console.log("q4");
+           console.log(users+"\n");
+           return;
+});
