3e3500c090a9fe8e6f56059d81a5296f134c136e
JunHyen Mun <impelfin@gmail.com>
2022-01-11 09:36:49 +0900

    .

--- python/data_get.py
+++ python/data_get.py
@@ -5,48 +5,46 @@ import requests
 import pandas as pd
 import xmltodict
 import json
-# 위에서 20210119 부터 20210120 까지 데이터를 불러옵니다
+
 key='B%2FNiJnYmkZV1%2FK7ulvZI4MoSXvCTDfNAd0Snw%2Bk6g4%2BbMk1LoGVhd75DJahjv4K35Cr9jh9RX0j%2BM89grKBYsw%3D%3D'
 url = f'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey={key}&'
 queryParams = urlencode({ quote_plus('pageNo') : 1,
                           quote_plus('numOfRows') : 10,
-                          quote_plus('startCreateDt') : '20220101',
-                          quote_plus('endCreateDt') : '20220110'})
+                          quote_plus('startCreateDt') : '20200101',
+                          quote_plus('endCreateDt') : '20220111'})
 url2 = url + queryParams
 response = urlopen(url2)
-# print(type(response)) # HTTPSresponse
 results = response.read().decode("utf-8")
-# print(type(results))   # str
 results_to_json = xmltodict.parse(results)
 data = json.loads(json.dumps(results_to_json))
 print(type(data))   # dic
 print(data)
-# {'accDefRate': '1.5093139972', 'accExamCnt': '4269316', 'accExamCompCnt': '4092389', 'careCnt': '17897',
-# 'clearCnt': '42953', 'createDt': '2021-01-01 09:36:53.691', 'deathCnt': '917', 'decideCnt': '61767',
-# 'examCnt': '176927', 'resutlNegCnt': '4030622', 'seq': '372',
-# 'stateDt': '20210101', 'stateTime': '00:00', 'updateDt': '2021-01-03 10:35:39.056'}
+
 corona=data['response']['body']['items']['item']
 #추가하고 싶은 리스트 생성
-Date=[]
-Cnt=[]
-clear_cnt=[]
-care_cnt=[]
+state_dt=[]
+state_time=[]
+decide_cnt=[]
 death_cnt=[]
-exam_cnt=[]     # examCnt   검사중
+acc_exam_cnt=[]
+acc_def_rate=[]
+create_dt=[]
+update_dt=[]
+
 for i in corona:
-    Date.append(i['stateDt'])  #'stateDt': '20200801'
-    Cnt.append(i['decideCnt'])  # decideCnt': '14336'   누적확진자
-    clear_cnt.append(i['clearCnt'])   # 13233           격리 해제환자
-    care_cnt.append(i['careCnt'])     # 802             치료중 환자
-    death_cnt.append(i['deathCnt'])    #301             사망자 수
-df=pd.DataFrame([Date,Cnt,clear_cnt,care_cnt,death_cnt]).T
-df.columns=['날짜','누적확진자','격리해제환자','치료중환자','사망자수']
-df=df.sort_values(by='날짜', ascending=True)
-# df.columns=['Date','acc_cnt','clear_cnt','care_cnt','death_cnt']
-# df=df.sort_values(by='Date', ascending=True)
-# df.head()
-# df.info()
-# df.describe()
+    state_dt.append(i['stateDt'])      # stateDt : 20200801 기준일
+    state_time.append(i['stateTime'])  # stateTime : 00:00 기준시간
+    decide_cnt.append(i['decideCnt'])  # decideCnt : 14336 확진자 수
+    death_cnt.append(i['deathCnt'])    # deathCnt : 75 사망자 수
+    acc_exam_cnt.append(i['accExamCnt'])   # accExamCnt : 268212 누적 검사 수
+    acc_def_rate.append(i['accDefRate'])     # accDefRate : 3.2396602365 누적 확진률
+    create_dt.append(i['createDt'])     # createDt : 2020-03-15 10:01:22.000 등록일시분초
+    update_dt.append(i['updateDt'])     # updateDt : null 수정일시분초
+
+df=pd.DataFrame([state_dt,state_time,decide_cnt,death_cnt,acc_exam_cnt,acc_def_rate,create_dt,update_dt]).T
+df.columns=['기준일','기준시간','확진자 수','사망자 수','누적 검사 수','누적 확진률','등록일시분초','수정일시분초']
+df=df.sort_values(by='기준일', ascending=True)
+
 # csv 파일 생성
 df.to_csv('sample.csv')
 # 메모장
