6062defa1519e4d86bc1dd889de56d52d27fb8d4
JunHyen Mun <impelfin@gmail.com>
2022-02-10 12:03:23 +0900

    .

--- api/request_axios.js
+++ api/request_axios.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 axios
-  .post('https://example.com/todos', {
+  .get('https://example.com/todos', {
     todo: 'Buy the milk'
   })
   .then(res => {
---
+++ node/testapi/routes/restful.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+const axios = require('axios')
+const https = require('https')
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+const users = [
+  { id:1, name: "User1"},
+  { id:2, name: "User2"},
+  { id:3, name: "User3"}
+];
+
+// simple api
+app.get("/Hello", (req, res) => {
+  res.send("Hello World!!");
+});
+
+// request param X, response O
+app.get("/api/users", (req, res) => {
+  const options = {
+    hostname: '54.180.44.109',
+    port: 3000,
+    path: '/api/users',
+    method: 'GET'
+  }
+  req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`)
+
+    res.on('data', d => {
+      process.stdout.write(d)
+    })
+  })
+  req.on('error', error => {
+    console.error(error)
+  })
+
+  req.end();
+  res.json(res);
+})
+
+// Query param, request param O, response O
+app.get("/api/users/user", (req, res) => {
+  const user_id = req.query.user_id
+  const user = users.filter(data => data.id == user_id);
+  res.json({ok:false, users:user});
+})
+
+// path param, request param O, response O
+app.get("/api/users/:user_id", (req, res) => {
+  const user_id = req.params.user_id
+  const user = users.filter(data => data.id == user_id);
+  res.json({ok:false, users:user});
+})
+
+// post, request body, response O
+app.post("/api/users/userBody", (req, res) => {
+  const user_id = req.body.id
+  const user = users.filter(data => data.id == user_id);
+  res.json({ok:true, users:user});
+})
+
+// post, request body, response O
+app.post("/api/users/add", (req, res) => {
+  const { id, name } = req.body
+  const user = users.concat({id, name});
+  res.json({ok:true, users:user});
+})
+
+// put, request body, response O
+app.put("/api/users/update", (req, res) => {
+  const { id, name } = req.body
+  const user = users.map(data => {
+    if(data.id == id) data.name = name
+    return {
+      id : data.id,
+      name : data.name
+    }
+  })
+  res.json({ok:true, users:user});
+})
+
+// patch, request path param & body, response O
+app.patch("/api/user/update/:user_id", (req, res) => {
+  const { user_id } = req.params
+  const { name } = req.body
+  const user = users.map(data => {
+    if(data.id == user_id) data.name = name
+    return {
+      id : data.id,
+      name : data.name
+    }
+  })
+  res.json({ok:true, users:user});
+})
+
+// delete, request body, response O
+app.delete("/api/user/delete", (req, res) => {
+  const { user_id } = req.body
+  const user = users.filter(data => data.id != user_id);
+  res.json({ok:true, users:user});
+})
+
+module.exports = app;
