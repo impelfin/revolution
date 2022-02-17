f081615df629bc220afad585117bb689c948a90f
JunHyen Mun <impelfin@gmail.com>
2022-02-17 15:23:15 +0900

    final

--- node/esps/routes/esps.js
+++ node/esps/routes/esps.js
@@ -17,14 +17,14 @@ app.get("/Hello", (req, res) => {
 app.get("/get-sensor", (req, res) => {
   const temperature = req.query.temperature;
   console.log("value : "+temperature);
-  res.json({ok:get, value : temperature});
+  res.json({ok:"get", value : temperature});
 })
 
 // post, request body, response O
 app.post("/post-sensor", (req, res) => {
   const { sensor_number, value } = req.body;
   console.log("sensor_number : "+sensor_number+" / value : "+value);
-  res.json({ok:post, sensor_number:sensor_number, value : value});
+  res.json({ok:"post", sensor_number:sensor_number, value : value});
 })
 
 module.exports = app;
