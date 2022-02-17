f1467b58f3b7bcdbb767c1e7413efb2e1c701aa7
JunHyen Mun <impelfin@gmail.com>
2022-02-17 10:40:53 +0900

    up

---
+++ node/esps/routes/esps.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+// simple api
+app.get("/Hello", (req, res) => {
+  res.send("Hello World!!");
+});
+
+// post, request body, response O
+app.post("/postplain", (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+})
+
+// post, request body, response O
+app.get("/postplainget", (req, res) => {
+  res.json(req.body);
+})
+
+
+module.exports = app;
