1416ee59d9cf14d0e254183a43557f11cd248e23
JunHyen Mun <impelfin@gmail.com>
2022-03-22 15:11:44 +0900

    .

--- node/s3/routes/main.js
+++ node/s3/routes/main.js
@@ -12,8 +12,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 
 const AWS = require('aws-sdk');
-const ID = 'process.env.ID';
-const SECRET = 'process.env.SECRET';
+const ID = process.env.ID;
+const SECRET = process.env.SECRET;
 const BUCKET_NAME = 'my-028bucket';
 const MYREGION = 'ap-northeast-2'
 const s3 = new AWS.S3({accessKeyId: ID, secretAccessKey: SECRET, region: MYREGION});
--- node/s3data/upload.js
+++ node/s3data/upload.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const env = require("dotenv").config({ path: "../../../.env"});
 
 const AWS =  require('aws-sdk');
-const ID = 'process.env.ID';
-const SECRET = 'process.env.SECRET';
+const ID = process.env.ID;
+const SECRET = process.env.SECRET;
 const BUCKET_NAME = 'my-028bucket';
 const MYREGION = 'ap-northeast-2'
 const s3 = new AWS.S3({accessKeyId: ID, secretAccessKey: SECRET, region: MYREGION});
