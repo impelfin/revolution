0621cf963522cc85439e64747c65c8c7975152e0
JunHyen Mun <impelfin@gmail.com>
2022-04-10 21:00:15 +0900

    new

---
+++ react/hello_props/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    hello: 'hello app js!'
+  };
+  
+  render() {
+    return <div className="App">{this.state.hello}</div>;
+  }
+}
+
+export default App;
