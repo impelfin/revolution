ac131172474ed650acfbe9de4e50a96b1954d559
JunHyen Mun <impelfin@gmail.com>
2022-04-12 18:27:46 +0900

    .

--- react/hello_props/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(
-  <App message="Hello message" />,
-  document.getElementById('root')
-);
